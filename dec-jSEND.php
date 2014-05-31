f5431a592ce9f35118e6a177cad5eeafa7ce237f
g3n1k <g3n1k@yahoo.com>
2014-06-01 05:11:07 +0700

    Update dec-jSEND.php
    remove warning and error from class

--- dec-jSEND.php
+++ dec-jSEND.php
@@ -1,5 +1,7 @@
 <?php
 require_once('jsend.class.php');
+ini_set('display_errors', 0);
+ini_set('display_warnings', 0);
 $enc = $_POST['encrypted'];
 $jSEND = new jSEND(); 
 $str = $jSEND->getData($enc);
